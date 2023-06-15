633f7089496d56ab68466fc0b2173c4a0947b4c3
cindykandie <cindyjk6@gmail.com>
2023-06-15 19:26:23 +0300

    Add services link

--- src/components/services.js
+++ src/components/services.js
@@ -9,7 +9,7 @@ export default function Services() {
     setIsModalOpen(true);
   }
   return (
-    <section className='border-y border-gray-400 p-6'>
+    <section id='services' className='border-y border-gray-400 p-6'>
       <h2 className='text-center text-4xl font-bold mt-6'>Services</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mx-2 lg:mx-6">
         {services.map((service) => (
